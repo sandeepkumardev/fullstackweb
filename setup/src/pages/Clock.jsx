4a06c09b695e9d2e7168747398659a1aec0be21b
sandeep k. <buildbysandeep@gmail.com>
2026-07-24 11:52:04 +0530

    feat: update clock

--- setup/src/pages/Clock.jsx
+++ setup/src/pages/Clock.jsx
@@ -1,14 +1,76 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Clock = () => {
-  const [time, setTime] = React.useState(new Date().toLocaleTimeString());
+  // const [second, setSecond] = useState(0);
+  // const [minute, setMinute] = useState(0);
+  // const [hour, setHour] = useState(0);
+
+  const [time, setTime] = useState({
+    second: 57,
+    minute: 58,
+    hour: 23,
+  });
 
   useEffect(() => {
-    setInterval(() => {
-      setTime(new Date().toLocaleTimeString());
+    const timer = setInterval(() => {
+      // setSecond((prev) => {
+      //   if (prev === 59) {
+      //     setMinute((prev) => {
+      //       if (prev === 59) {
+      //         setHour((prev) => {
+      //           if (prev === 23) {
+      //             return 0;
+      //           }
+      //           return prev + 1;
+      //         });
+      //         return 0;
+      //       }
+      //       return prev + 1;
+      //     });
+      //     return 0;
+      //   }
+      //   return prev + 1;
+      // });
+
+      setTime((prev) => {
+        let { second, minute, hour } = prev;
+
+        second++;
+
+        if (second === 60) {
+          second = 0;
+          minute = minute + 1;
+        }
+
+        if (minute === 60) {
+          minute = 0;
+          hour = hour + 1;
+        }
+
+        if (hour === 24) {
+          hour = 0;
+        }
+
+        return {
+          second,
+          minute,
+          hour,
+        };
+      });
     }, 1000);
+
+    return () => clearInterval(timer);
   }, []);
-  return <div>{time}</div>;
+
+  const fs = (str) => {
+    return String(str).padStart(2, "0");
+  };
+
+  return (
+    <div>
+      {fs(time.hour)}:{fs(time.minute)}:{fs(time.second)}
+    </div>
+  );
 };
 
 export default Clock;
